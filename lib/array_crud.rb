c47e129134908f1b0fb3e9b215cdc9c3d0e50eee
Alex S Grimes <alex@alexgrimes.com>
2020-03-31 01:40:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_element_from_index = ["zero", "one", "two", "three"]
+  retrieve_element_from_index = ["I", "am", "two", "three"]
   retrieve_element_from_index[1]
 end
 
